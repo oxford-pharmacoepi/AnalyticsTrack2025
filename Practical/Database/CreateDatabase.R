4d4ba2a34b9db7d119f974d49d3c9e3b27a16d9e
catalamarti <marticatalasabate@gmail.com>
2025-06-20 01:08:04 +0100

    Create database

--- Practical/Database/CreateDatabase.R
+++ Practical/Database/CreateDatabase.R
@@ -1,52 +1,72 @@
 
-datasets <- omock::availableMockDatasets() |>
-  purrr::keep(\(x) x != "empty_cdm")
+cdm <- omock::mockCdmFromDataset(datasetName = "synthea-covid19-200k")
 
-for (nm in datasets) {
-  cli::cli_inform("{.strong Analysisng data set: {.pkg {nm}}}:")
-  cdm <- omock::mockCdmFromDataset(datasetName = nm)
+# read extra condition occurrence rows
+conditionOccurrence <- readr::read_csv(file = here::here("Database", "condition_occurrence_extra_rows.csv"), show_col_types = FALSE) |>
+  dplyr::mutate(
+    condition_occurrence_id = as.integer(dplyr::row_number() + max(cdm$condition_occurrence$condition_occurrence_id)),
+    person_id = as.integer(person_id),
+    condition_concept_id = as.integer(condition_concept_id),
+    condition_start_date = as.Date(condition_start_date),
+    condition_start_datetime = condition_start_date,
+    condition_end_date = condition_start_date,
+    condition_end_datetime = condition_start_date,
+    condition_type_concept_id = 38000175L,
+    condition_status_concept_id = 0L,
+    stop_reason = NA_character_,
+    provider_id = NA_integer_,
+    visit_occurrence_id = 0L,
+    visit_detail_id = 0L,
+    condition_source_value = NA_character_,
+    condition_source_concept_id = 0L,
+    condition_status_source_value = NA_character_
+  )
+cdm$condition_occurrence <- cdm$condition_occurrence |>
+  dplyr::union_all(conditionOccurrence)
 
-  # driver
-  drv <- duckdb::duckdb(dbdir = here::here("Database", paste0(nm, ".duckdb")))
-  con <- duckdb::dbConnect(drv = drv)
-  src <- CDMConnector::dbSource(con = con, writeSchema = "main")
-  cdm <- omopgenerics::insertCdmTo(cdm = cdm, to = src)
+# read extra drug exposure rows
+drugExposure <- readr::read_csv(file = here::here("Database", "drug_exposure_extra_rows.csv"), show_col_types = FALSE) |>
+  dplyr::mutate(
+    drug_exposure_id = as.integer(dplyr::row_number() + max(cdm$drug_exposure$drug_exposure_id)),
+    person_id = as.integer(person_id),
+    drug_concept_id = as.integer(drug_concept_id),
+    drug_exposure_start_date = as.Date(drug_exposure_start_date),
+    drug_exposure_end_date = as.Date(drug_exposure_end_date),
+    drug_exposure_start_datetime = drug_exposure_start_date,
+    drug_exposure_end_datetime = drug_exposure_end_date,
+    verbatim_end_date = drug_exposure_end_date,
+    drug_type_concept_id = 32869L,
+    stop_reason = NA_character_,
+    refills = 0L,
+    quantity = as.numeric(quantity),
+    days_supply = as.integer(days_supply),
+    sig = NA_character_,
+    route_concept_id = 0L,
+    lot_number = "0",
+    provider_id = 0L,
+    visit_occurrence_id = 0L,
+    visit_detail_id = 0L,
+    drug_source_value = NA_character_,
+    drug_source_concept_id = 0L,
+    route_source_value = NA_character_,
+    dose_unit_source_value = NA_character_
+  )
+cdm$drug_exposure <- cdm$drug_exposure |>
+  dplyr::union_all(drugExposure)
 
-  summary <- character()
+dbdir <- here::here("Database", "TestDatabase.duckdb")
 
-  nInd <- cdm$person |>
-    dplyr::tally() |>
-    dplyr::pull()
-  summary <- c(summary, paste0("Number Individuals = ", nInd), "")
-
-  conditions <- cdm$condition_occurrence |>
-    PatientProfiles::addConceptName(column = "condition_concept_id", nameStyle = "concept_name") |>
-    dplyr::group_by(concept_name) |>
-    dplyr::summarise(n = dplyr::n_distinct(person_id)) |>
-    dplyr::collect() |>
-    dplyr::arrange(dplyr::desc(n)) |>
-    utils::head(5) |>
-    dplyr::mutate(mes = paste0(concept_name, ": ", n)) |>
-    dplyr::pull("mes")
-  names(conditions) <- rep("*", length(conditions))
-  summary <- c(summary, conditions, "")
-
-  medications <- cdm$drug_exposure |>
-    PatientProfiles::addConceptName(column = "drug_concept_id", nameStyle = "concept_name") |>
-    dplyr::group_by(concept_name) |>
-    dplyr::summarise(n = dplyr::n_distinct(person_id)) |>
-    dplyr::collect() |>
-    dplyr::arrange(dplyr::desc(n)) |>
-    utils::head(5) |>
-    dplyr::mutate(mes = paste0(concept_name, ": ", n)) |>
-    dplyr::pull("mes")
-  names(medications) <- rep("*", length(medications))
-  summary <- c(summary, medications, "")
-
-  # message
-  cat(c(summary, ""), sep = "\n")
-
-  # disconnect
-  duckdb::dbDisconnect(conn = con)
-  duckdb::duckdb_shutdown(drv)
+# delete database if exist
+if (file.exists(dbdir)) {
+  drv <- duckdb::duckdb(dbdir = dbdir)
+  suppressWarnings(duckdb::duckdb_shutdown(drv = drv))
+  unlink(x = dbdir)
 }
+
+# copy data to duckdb database
+drv <- duckdb::duckdb(dbdir = dbdir)
+con <- duckdb::dbConnect(drv = drv)
+DBI::dbExecute(con, "CREATE SCHEMA results")
+src <- CDMConnector::dbSource(con = con, writeSchema = "main")
+cdm <- CDMConnector::insertCdmTo(cdm = cdm, to = src)
+CDMConnector::cdmDisconnect(cdm = cdm)
--- Practical/MyStudy.R
+++ Practical/MyStudy.R
@@ -1,18 +1,25 @@
-
-library(omock)
+# load libraries ----
 library(CDMConnector)
 library(duckdb)
 library(here)
 
-# create the database
+# create the database ----
+# every time that you source the database will be created again
+# uncomment to run!
 # source(here("Database", "CreateDatabase.R"))
 
-# create a cdm reference
-con <- dbConnect(drv = duckdb(dbdir = here("Database", "myDatabase.duckdb")))
+# create a cdm reference ----
+# create a connection to the database
+con <- dbConnect(drv = duckdb(dbdir = here("Database", "TestDatabase.duckdb")))
+
+# create the cdm object
 cdm <- cdmFromCon(
   con = con,
   cdmSchema = "main",
   writeSchema = "results",
   writePrefix = "rwess_",
-  cdmName = ""
+  cdmName = "SummerSchool2025"
 )
+cdm
+
+# characterise the database ----
